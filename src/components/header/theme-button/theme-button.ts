204f8d7c14d4777053287be732d11755d7e6444a
RyanQueirozS <ryanqueiroz@proton.me>
2026-02-16 10:40:22 -0300

    feature: added shake and spin animation to logo

--- src/components/header/theme-button/theme-button.ts
+++ src/components/header/theme-button/theme-button.ts
@@ -20,10 +20,32 @@ export class ThemeButtonComponent {
 
   private toggleTheme() {
     const html = document.documentElement
+    const logo = document.querySelector<HTMLElement>(".header-logo svg")
+
     const nextTheme = html.dataset.theme === "dark" ? "light" : "dark"
 
     html.dataset.theme = nextTheme
     localStorage.setItem("theme", nextTheme)
+
+    if (!logo) return
+
+    const chance = Math.floor((Math.random() * 3) + 1);
+
+    if (chance % 3 == 0) {
+
+      logo.classList.add("logo-spin")
+
+      setTimeout(() => {
+        logo?.classList.remove("logo-spin")
+      }, 1000)
+      return;
+    }
+
+    logo.classList.add("logo-shake")
+
+    setTimeout(() => {
+      logo?.classList.remove("logo-shake")
+    }, 500)
   }
 
   destroy() {
